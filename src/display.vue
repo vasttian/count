58b6489b0033f354fe79f4d94907989b09d71fe2
tianyiming <tianyiming26@gmail.com>
2017-03-29 10:23:54 +0800

    this is also fix

--- src/display.vue
+++ src/display.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-		<h3>Count is {{count}}</h3>
-		
+		<h3>Count is {{ count }}</h3>
+
 		<!-- <p>组件自己的内部计算属性{{ localComputed }}</p> -->
-		<h3>AnoterIncrement is {{countAnother}}</h3>
+		<h3>AnoterIncrement is {{ countAnother }}</h3>
 	</div>
 </template>
 
